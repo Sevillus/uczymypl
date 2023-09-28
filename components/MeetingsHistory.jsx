ad63af15f78f46548e7faab2df9e6743b7b6c496
Sevillus <rafal.czarnecki00@gmail.com>
2023-09-28 11:35:32 +0200

    Adding  non paid lessons warning

--- components/MeetingsHistory.jsx
+++ components/MeetingsHistory.jsx
@@ -3,37 +3,51 @@ import React, {useEffect, useState} from "react";
 import convertDate from "../utils/convertDate";
 import dayjs from "dayjs";
 import MeetingHistoryInfo from "./meetingHistoryInfo";
+import ReportGmailerrorredIcon from '@mui/icons-material/ReportGmailerrorred';
 
 const MeetingsHistory = ({ meetingHistory, setEarnedThisMonth }) => {
   let prevDay = null;
-
-
+  const [paymentInfo, setPaymentInfo] = useState([])
+    console.log(paymentInfo)
 
   return (
-    <div className={"w-full h-72 border-2 overflow-y-scroll "}>
-      {meetingHistory
-        .slice()
-        .reverse()
-        .map((student, index) => {
-          const isSameDay =
-            dayjs(student.nextMeeting).day() !== dayjs(prevDay).day();
-          prevDay = student.nextMeeting;
-
-          return (
-            <div key={index} className={"flex gap-2 flex-col p-2"}>
-              {isSameDay && (
-                <div className={"border-b-2"}>
-                  <p className={"font-medium"}>
-                    {convertDate(student.nextMeeting).dayConverted}
-                  </p>
+      <div >
+        <div className={"flex-between"}>
+        <h1 className={"mb-2 text-lg font-semibold"}>Historia płatności</h1>
+            {!paymentInfo.length ?(
+                <div className={"flex gap-2 text-rose-600 "}>
+                    <ReportGmailerrorredIcon />
+                    <p>Nie opłacone zajęcia!</p>
                 </div>
-              )}
-            <MeetingHistoryInfo student={student} key={index} meetingHistory={meetingHistory} setEarnedThisMonth={setEarnedThisMonth}/>
+            ):""}
 
-            </div>
-          );
-        })}
-    </div>
+
+        </div>
+        <div className={"w-full h-72 border-2 overflow-y-scroll "}>
+          {meetingHistory
+              .slice()
+              .reverse()
+              .map((student, index) => {
+                const isSameDay =
+                    dayjs(student.nextMeeting).day() !== dayjs(prevDay).day();
+                prevDay = student.nextMeeting;
+
+                return (
+                    <div key={index} className={"flex gap-2 flex-col p-2"}>
+                      {isSameDay && (
+                          <div className={"border-b-2"}>
+                            <p className={"font-medium"}>
+                              {convertDate(student.nextMeeting).dayConverted}
+                            </p>
+                          </div>
+                      )}
+                      <MeetingHistoryInfo student={student} key={index} meetingHistory={meetingHistory} setEarnedThisMonth={setEarnedThisMonth} setPaymentInfo={setPaymentInfo}/>
+                    </div>
+                );
+              })}
+        </div>
+      </div>
+
   );
 };
 
--- components/meetingHistoryInfo.jsx
+++ components/meetingHistoryInfo.jsx
@@ -1,63 +1,75 @@
-"use client"
-import React, {useEffect, useState} from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
 import ClearIcon from "@mui/icons-material/Clear";
 import DoneIcon from "@mui/icons-material/Done";
 
-const MeetingHistoryInfo = ({student,meetingHistory, setEarnedThisMonth}) => {
-    const [payment, setPayment] = useState(student.isPaid)
+const MeetingHistoryInfo = ({
+  student,
+  meetingHistory,
+  setEarnedThisMonth,
+  setPaymentInfo
+}) => {
+  const [payment, setPayment] = useState(student.isPaid);
+  const [earned, setEarned] = useState(0);
 
 
 
-    const [earned, setEarned] = useState(0);
+  useEffect(() => {
+    let counter = 0;
+    meetingHistory.forEach((student) => {
+      if(student.isPaid) {
+        counter = counter + student.price
+        setPaymentInfo(prev => [...prev, 1])
+      }
+    });
+    setEarned(counter);
+    setEarnedThisMonth(counter);
+  }, [meetingHistory, setEarnedThisMonth]);
 
-    useEffect(() => {
-        let counter = 0;
-        meetingHistory.forEach(student => {
-            student.isPaid ? counter = counter + student.price:"";
-        });
-        setEarned(counter);
-        setEarnedThisMonth(counter);
-    }, [meetingHistory, setEarnedThisMonth]);
+  const addPayment = async (student, isPaid) => {
+    setPayment((prev) => !prev);
 
-    const addPayment = async (student, isPaid) => {
-        setPayment((prev) => !prev);
-        let updatedEarned = 0
-        if (!payment) {
-            updatedEarned = earned + student.price;
-        } else {
-            updatedEarned = earned - student.price;
-        }
-        setEarned(updatedEarned);
-        setEarnedThisMonth(updatedEarned);
-        await fetch(`api/meeting-history`, {
-            method: "POST",
-            body: JSON.stringify({
-                isPaid: isPaid,
-                id: student._id,
-            }),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-    };
+    let updatedEarned = 0;
+    if (!payment) {
+      updatedEarned = earned + student.price;
+      setPaymentInfo(prev =>[...prev,1])
+    } else {
+      updatedEarned = earned - student.price;
+    }
+    setEarned(updatedEarned);
+    setEarnedThisMonth(updatedEarned);
+    await fetch(`api/meeting-history`, {
+      method: "POST",
+      body: JSON.stringify({
+        isPaid: isPaid,
+        id: student._id,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
 
-
-    return (
-        <div className={"flex-between px-4"}>
-            <p>{student.name}</p>
-            <div className={"flex gap-4"}>
-                <p className={payment ? "text-green-600" : "text-rose-600"}>
-                    {student.price},00zł
-                </p>
-                {payment ? (
-                    <ClearIcon
-                        className={"w-4"} onClick={() => addPayment(student,  false)}
-                    />
-                ) : (
-                    <DoneIcon className={"w-4"} onClick={() => addPayment(student, true)} />
-                )}
-            </div>
-        </div>
-    )
-}
-export default MeetingHistoryInfo
+  return (
+    <div className={"flex-between px-4"}>
+      <p>{student.name}</p>
+      <div className={"flex gap-4"}>
+        <p className={payment ? "text-green-600" : "text-rose-600"}>
+          {student.price},00zł
+        </p>
+        {payment ? (
+          <ClearIcon
+            className={"w-4"}
+            onClick={() => addPayment(student, false)}
+          />
+        ) : (
+          <DoneIcon
+            className={"w-4"}
+            onClick={() => addPayment(student, true)}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+export default MeetingHistoryInfo;
