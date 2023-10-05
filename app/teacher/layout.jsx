6245eda43a83c05af6284dd0d17d9d0dbd5e6f17
Sevillus <rafal.czarnecki00@gmail.com>
2023-10-05 13:17:27 +0200

    changing fetch api path it could be problem

--- app/teacher/layout.jsx
+++ app/teacher/layout.jsx
@@ -13,9 +13,9 @@ const Layout = async ({ children }) => {
     const session = await getServerSession(authOptions)
 
 
-    if (!session || session?.user.role !== "teacher" ) {
-        redirect('/')
-    }
+    // if (!session || session?.user.role !== "teacher" ) {
+    //     redirect('/')
+    // }
 
     return (
         <>
