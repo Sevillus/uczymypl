7260b581b6d7d36143b3b0894e4ab3571865b618
Sevillus <rafal.czarnecki00@gmail.com>
2023-10-05 11:55:30 +0200

    elo

--- app/api/getUserData/route.js
+++ app/api/getUserData/route.js
@@ -9,12 +9,12 @@ import { sortByDate } from "../../../utils/sortByDate";
 import {daysOfWeek, daysOfWeekNames} from "../../../constants/months";
 
 export async function GET(req, res) {
-
+  await connectToDB();
 
   const session = await getServerSession(authOptions);
 
   try {
-    if (req.method === "GET") {
+    if (req.method === "GET" && session?.user) {
       // Pobierz dane użytkownika na podstawie sesji
       const sessionUser = await User.findOne({ email: session?.user.email });
 
