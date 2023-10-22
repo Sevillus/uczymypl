cfd6f6ef058d1327fceb91d3c655bc1a5e1f8fef
Sevillus <rafal.czarnecki00@gmail.com>
2023-10-22 20:43:30 +0200

    Changing meeting's popup style

--- components/MeetingPopUp.jsx
+++ components/MeetingPopUp.jsx
@@ -39,16 +39,20 @@ const MeetingPopUp = ({ user , setMeetingHistory}) => {
     <div className={"w-full h-full absolute flex-center top-0 right-0 backgroundShadow "}>
       <div
         className={
-          "py-6 px-4  border-2 lg:w-3/12 h-fit bg-white z-10 drop-shadow-xl flex flex-col gap-4 z-30  "
+          "py-6 px-4  border-2 lg:w-3/12 min-h-[40vh]  bg-white z-10 drop-shadow-xl flex flex-col gap-4 z-30  "
         }
       >
         <h1 className={"text-xl font-bold"}>Cześć {session?.user.name.split(" ")[0]} !</h1>
         <span>Czy poniższe zajęcia się odbyły?</span>
         {lastMeetings.map((student, index) => (
-          <div key={index} className={"flex-between w-full"}>
-            <p>
-              {student.name}, {student.day} {dayjs(student.nextMeeting).format("D.MM")} o {student.time}
-            </p>
+          <div key={index} className={"flex-between w-full border-b-2 py-2"}>
+            <div>
+              <p className={"font-semibold "}>
+                {student.name}
+              </p>
+              <p className={"text-slate-400"}> {student.day} {dayjs(student.nextMeeting).format("D.MM")} o {student.time}</p>
+            </div>
+
             <div className={"w-2/12 flex-between"}>
               <button>
                 <ClearIcon onClick={() => confirmHandler(student, false)} className={"text-rose-600"}/>
