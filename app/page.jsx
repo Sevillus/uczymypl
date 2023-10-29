84f25134df03341cacaeb54788e944398ffc76b3
Sevillus <rafal.czarnecki00@gmail.com>
2023-10-29 14:55:14 +0100

    Styling home page

--- app/page.jsx
+++ app/page.jsx
@@ -6,18 +6,26 @@ const HomePage = () => {
     return (
         <div className={"h-fit bg-gradient px-56 text-white"}>
             <HomePageTitle />
-            <h2 className={"text-4xl font-semibold"}>Zarządzaj swoimi zajęciami</h2>
-            <div className={"w-full h-[700px] flex-between"}>
-                <div className={"w-4/12 flex-column gap-10"}>
+            <div className={"w-full h-[700px]"}>
+                <h2 className={"text-4xl font-semibold"}>Zarządzaj swoimi zajęciami</h2>
+                <div className={"flex-between"}>
+                    <div className={"w-4/12 flex-column gap-10"}>
 
-                    <p className={"text-2xl leading-10 text-slate-200 pl-4"}>
-                        Dzięki naszej stronie internetowej,
-                        możesz z łatwością monitorować kiedy oraz z kim masz poszczególne spotkanie, czy uczeń zapłacił za zajęcia
-                        oraz jak blisko jesteś ukończenia miesięcznego targetu.
-                    </p>
+                        <p className={"text-2xl leading-10 text-slate-200 pl-4"}>
+                            Dzięki naszej stronie internetowej,
+                            możesz z łatwością monitorować kiedy oraz z kim masz poszczególne spotkanie, czy uczeń zapłacił za zajęcia
+                            oraz jak blisko jesteś ukończenia miesięcznego targetu.
+                        </p>
+                    </div>
+                    <div className={"relative"}>
+                        <Image src={"/dashboard.jpg"} alt={"dashboard"} width={"500"} height={"700"}  className={"shadow-2xl rounded-lg"}/>
+                        <Image src={"/mobile.jpg"} alt={"dashboard"} width={"200"} height={"200"}  className={"shadow-2xl rounded-lg -bottom-32 -left-32 absolute"}/>
+
+                    </div>
+
                 </div>
-                <Image src={"/dashboard.jpg"} alt={"dashboard"} width={"1100"} height={"1000"} />
             </div>
+
         </div>
     );
 };
