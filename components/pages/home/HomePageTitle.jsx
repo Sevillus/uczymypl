d631b6133ab1cbd6e90ed9bf1c40fe39e08210ec
Sevillus <rafal.czarnecki00@gmail.com>
2023-11-02 21:00:58 +0100

    removing typos

--- components/pages/home/HomePageTitle.jsx
+++ components/pages/home/HomePageTitle.jsx
@@ -9,7 +9,7 @@ const HomePageTitle = () => {
             <div className={"w-12/12 flex-center"}>
                 <div className={"w-full lg:w-5/12 flex-column  items-center gap-6"}>
                     <h1 className={"font-damion text-6xl lg:text-8xl font-normal"}>Uczymy.pl</h1>
-                    <p className={"text-2xl leading-10 text-center text-slate-200"}>Łączymy wszystkie niezbędna narzędzia oraz zadania korepetytora w jednym miejscu. Nie zwlekaj, załóż w pełni darmowe konto już dziś i ułatw sobie prace!</p>
+                    <p className={"text-2xl leading-10 text-center text-slate-200"}>Łączymy wszystkie niezbędne narzędzia oraz zadania korepetytora w jednym miejscu. Nie zwlekaj, załóż w pełni darmowe konto już dziś i ułatw sobie prace!</p>
                     <div className={"w-full flex-center"}>
                         <button className={"border p-4 rounded-lg flex gap-4 hover:translate-x-6 easy-in-out duration-300"}
                                 type="button"
