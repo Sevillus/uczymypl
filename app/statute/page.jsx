d28320fc74b187041aea3c5f074102ae2770f7be
Sevillus <rafal.czarnecki00@gmail.com>
2023-11-03 18:52:47 +0100

    Created statute page, contains page's statute and privacy policy

---
+++ app/statute/page.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+
+const Statue = () => {
+    return (
+        <div className={"w-full h-full flex-center flex-col"}>
+            <div className={"w-10/12 lg:w-4/12 flex-column gap-4"}>
+                <h1 className={"text-xl font-bold my-4"}>Polityka prywatności i Warunki korzystania z serwisu</h1>
+                <div>
+                    <h2 className={"text-lg font-semibold"}>1. Cele przetwarzania danych osobowych</h2>
+                    <p className={"my-4"}>Uczymy.pl przetwarza dane osobowe użytkowników w celu:</p>
+                    <p>- Umożliwienia logowania za pomocą konta Google i zarządzania kontem użytkownika.</p>
+                    <p>- Komunikacji z użytkownikami w celu dostarczania informacji, aktualizacji i odpowiedzi na pytania</p>
+                    <p>- Przechowywania danych uczniów w celu umożliwienia korzystania z funkcji serwisu.</p>
+                </div>
+                <div>
+                    <h2 className={"text-lg font-semibold"}>2. Podstawa prawna przetwarzania danych</h2>
+                    <p className={"my-4"}>Podstawą prawną przetwarzania danych osobowych na naszej stronie internetowej jest zgoda użytkownika oraz niezbędność przetwarzania danych do realizacji umowy.</p>
+                </div>
+                <div>
+                    <h2 className={"text-lg font-semibold"}>3. Przechowywanie danych osobowych</h2>
+                    <p className={"my-4"}>Dane osobowe użytkowników oraz danych ich uczniów będą przechowywane w bezpieczny sposób, z ograniczonym dostępem tylko dla upoważnionych osób.</p>
+                </div>
+                <div>
+                    <h2 className={"text-lg font-semibold"}>4. Okres przechowywania danych</h2>
+                    <p className={"my-4"}>Dane osobowe będą przechowywane tylko przez okres niezbędny do realizacji celów przetwarzania i zgodnie z obowiązującymi przepisami.</p>
+                </div>
+                <div>
+                    <h2 className={"text-lg font-semibold"}>5. Prawa użytkowników</h2>
+                    <p className={"my-4"}> Użytkownicy mają prawo dostępu do swoich danych osobowych, ich poprawiania, usuwania lub ograniczenia przetwarzania. Mogą również skorzystać z prawa do przenoszenia danych oraz złożyć skargę do organu nadzorczego ds. ochrony danych.</p>
+                </div>
+                <div>
+                    <h2 className={"text-lg font-semibold"}>6. Zabezpieczenia danych</h2>
+                    <p className={"my-4"}>Zastosowano odpowiednie środki zabezpieczenia danych osobowych przed nieuprawnionym dostępem, utratą, uszkodzeniem lub ujawnieniem.</p>
+                </div>
+                <div>
+                    <h2 className={"text-lg font-semibold"}>7. Pliki cookies</h2>
+                    <p className={"my-4"}> Strona internetowa może używać plików cookies. Użytkownicy zostaną poinformowani o tym fakcie i będą mieli możliwość wyrażenia zgody na ich użycie.</p>
+                </div>
+                <div>
+                    <h2 className={"text-lg font-semibold"}>8. Kontakt</h2>
+                    <p className={"my-4"}>W przypadku pytań lub w celu realizacji praw użytkowników, prosimy o kontakt pod adresem e-mail rafal.czarnecki00@gmail.com. </p>
+                </div>
+                <h1 className={"text-xl font-bold my-4"}>Warunki korzystania z serwisu</h1>
+                <div>
+                    <h2 className={"text-lg font-semibold"}>1. Korzystanie z serwisu</h2>
+                    <p className={"my-4"}> Korzystanie z naszego serwisu oznacza akceptację niniejszych Warunków korzystania.</p>
+                </div>
+                <div>
+                    <h2 className={"text-lg font-semibold"}>2. Logowanie za pomocą konta Google</h2>
+                    <p className={"my-4"}>Użytkownik zgadza się na logowanie za pomocą konta Google, co może wymagać dostępu do niektórych danych z tego konta. </p>
+                </div>
+                <div>
+                    <h2 className={"text-lg font-semibold"}>3. Odpowiedzialność</h2>
+                    <p className={"my-4"}>Nasza strona internetowa nie ponosi odpowiedzialności za treści i działania użytkowników. </p>
+                </div>
+                <div>
+                    <h2 className={"text-lg font-semibold"}>4. Zmiany w serwisie</h2>
+                    <p className={"my-4"}>Zastrzegamy sobie prawo do wprowadzania zmian w serwisie i Warunkach korzystania. </p>
+                </div>
+                <div className={"mb-10"}>
+                    <h2 className={"text-lg font-semibold"}>5. Ostateczność</h2>
+                    <p className={"my-4"}> Niniejsze dokumenty stanowią wyłączne źródło informacji dotyczących polityki prywatności oraz Warunków korzystania z naszego serwisu internetowego. Jeśli użytkownik nie zgadza się z tymi warunkami, nie powinien korzystać z serwisu.</p>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+export default Statue
--- components/globals/footer/Footer.jsx
+++ components/globals/footer/Footer.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className={"bg-slate-200  flex-center padding-y padding-x text-center"}>
+    <footer className={"bg-slate-200  flex-center padding-y padding-x text-center flex-col"}>
       <p className={"text-slate-500 text-sm"}>
         Właścicielem strony jest Uczymypl. Wszelkie zapytania prosimy kierować
         na adres uczymypl@kontakt.pl
+          Korzystając ze strony akceptujesz regulamin oraz politykę prywatności strony.
       </p>
+      <Link href={"/statute"}>Regulamin i polityka prywatności</Link>
+
     </footer>
   );
 };
