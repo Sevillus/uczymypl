8c6e134f408e63351b0fd217ae9ddb0b75c2299c
Sevillus <rafal.czarnecki00@gmail.com>
2023-11-08 20:51:06 +0100

    Schedule Page RWD

--- components/pages/teacher/schedule/ScheduleDesktop.jsx
+++ components/pages/teacher/schedule/ScheduleDesktop.jsx
@@ -3,7 +3,7 @@ import ScheduleBox from "./ScheduleBox";
 const ScheduleDesktop = ({ user, isLoading }) => {
 
   return (
-    <div className="flex flex-col hidden lg:flex lg:flex-row lg:justify-between  w-10/12  h-full lg:border">
+    <div className="flex flex-col hidden lg:flex lg:flex-row lg:justify-between  w-11/12  h-full lg:border">
       {user?.schedule?.map((day, index) => (
         <div key={index} className="flex-col w-full  h-full lg:border  bg-slate-50 hidden lg:flex">
           <div className="schedule__header">
